568f0183d29c22b18cb807cf1beabfbf13bb326b
unknown <eddyrivera62003@gmail.com>
2025-02-19 23:22:01 -0600

    Actualización Clase Mantenimiento
    Aplicación de getters y setters para la validación de datos, además de mensajes de error en caso de no recibir los datos esperados.

--- Mantenimiento.py
+++ Mantenimiento.py
@@ -4,5 +4,37 @@ class Mantenimiento:
         self.descripcion = descripcion
         self.precio = precio
 
+    @property
+    def fecha(self):
+        return self._fecha
+
+    @fecha.setter
+    def fecha(self, value):
+        if not value: 
+            raise ValueError("La fecha no puede estar vacia")
+        self._fecha = value
+
+    @property
+    def descripcion(self):
+        return self._descripcion
+
+    @descripcion.setter
+    def descripcion(self, value):
+        if not value:  
+            raise ValueError("Sin descripcion")
+        self._descripcion = value
+
+    @property
+    def precio(self):
+        return self.precio
+
+    @precio.setter
+    def precio(self, value):
+        if value < 0: 
+            raise ValueError("El precio no puede ser negativo.")
+        self.precio = value
+
     def __str__(self):
-        return f"Fecha: {self.fecha}, Descripción: {self.descripcion}, Costo: {self.precio}"
+        return f"Fecha: {self.fecha}, 
+                Descripción: {self.descripcion}, 
+                Costo: {self.precio}"
